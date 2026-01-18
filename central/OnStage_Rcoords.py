66e042f123cb78cb6971e88dc134e22f5f296b8a
George Deng <dcx11dcy17@gmail.com>
2026-01-18 17:01:14 -0500

    Refactor OnStage_Rcoords.py with new features

--- central/OnStage_Rcoords.py
+++ central/OnStage_Rcoords.py
@@ -1,25 +1,37 @@
 ### import libraries ###
-import sys
-import os
 import cv2
-import numpy as np
-import copy
+import socket
+import time
+import apriltag
 import math
+import numpy as np
 
-from picamera2 import Picamera2 #picam
+from CBF import drive
+from OnStage_Rcoords import updatePositions
+from WifiComms import write, read
 
+### setup camera ###
+# logitech
+cap = cv2.VideoCapture(0)  # 0 = default USB webcam
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+# picam
+#from picamera2 import Picamera2
+
+### AprilTag lib ###
 module_dir = os.path.abspath('/home/pi/onstage/python/apriltag/lib/python3.11/site-packages')
 sys.path.append(module_dir)
 import apriltag
 
 states = ["mining", "deposit", "withdraw", "plant", "none"]
 
-### objects/classes ###
+### objects ###
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    
+
     def distance_to(self, other_point):
         dx = self.x - other_point.x
         dy = self.y - other_point.y
@@ -27,33 +39,33 @@ class Point:
 
     def __str__(self):
         return f"Point({self.x}, {self.y})"
-    
+
 class robot:
     def __init__(self, IP, port, x, y, tag):
         self.IP = IP;
         self.port = port
         self.coords = Point(x, y)
+        self.water_level = 1
+        self.state = "none"
         self.tag = tag;
-        self.haswater = False
-        self.state = states[5]
-    def updatePosition(self, x, y):
-        self.coords = Point(x, y)
     def setTarget(x, y):
         self.target = Point(x, y)
-        
-    def changeWater():
-        self.haswater = not self.haswater
+    def depleteWater():
+        self.water_level -= 1
+    def restoreWater():
+        self.water_level += 1
     def atTarget():
-        return (self.coords.distance_to(self.target) < 0.5)
-    def changeState(int s):
+        return (math.sqrt((self.x-self.targetX)**2 + (self.y-self.targetY)**2) < 0.5)
+    
+    def setState(int s): #set state manually
         self.state = states[s]
-    def action():
+    def action(): #change state after completing current task
         if (self.atTarget == True):
             self.changeWater()
             for i in range(4):
                 if (self.state == states[i]):
                     self.state == states[(i + 1) % 4]
-                    
+
 class obstacle:
     def __init(self, x, y, radius):
         self.coords = Point(x, y)
@@ -71,35 +83,40 @@ class ice:
         self.coords = Point(x,y)
         self.level = level
 
-### field elements ###
-robots = [robot("192.168.32.172", 5000, 0, 0, 2)] 
+robots = [robot("192.168.32.172", 5000, 0, 0, 2), robot("192.168.32.172", 5000, 0, 0, 3)] 
 anchors = [Point(0, 0), Point(0, 0)]  #AT tag 0 and 1 respectively
 obstacles = [obstacle(0, 0, 0), obstacle(0, 0, 0), obstacle(0, 0, 0)]
 plants = [plant("<INSERT IP HERE>", 0, 0, 0, 0)]
 
 field_width = 1  #scales x dimension of relative coordinates
 field_length = 1  #scales y dimension of relative coordinates
-robotcount = 1  #predefined number of robots (prevent detection of extraneous tags)
+robotcount = 2  #predefined number of robots (prevent detection of extraneous tags)
 
+sockets = []
 
 ### functions ###
+def connect():
+    for robot in robots:
+        sockets.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+        sockets[-1].connect((robot.IP, robot.port))
+
 def updatePositions():
     # setup AprilTag detector
     options = apriltag.DetectorOptions(families="tag36h11")
     detector = apriltag.Detector(options)
-    
+
     # get image (logitech cam)
-#     ret, frame = cap.read()
-#     if not ret:
-#         print("Failed to grab frame")
-#         return
-#     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to grab frame")
+        return
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
     # get image (picam)
-    yuv420 = picam2.capture_array("lores")
-    rgb = cv2.cvtColor(yuv420, cv2.COLOR_YUV420p2RGB)
-    gray = cv2.cvtColor(yuv420, cv2.COLOR_YUV420p2GRAY)
+    #yuv420 = picam2.capture_array("lores")
+    #rgb = cv2.cvtColor(yuv420, cv2.COLOR_YUV420p2RGB)
+    #gray = cv2.cvtColor(yuv420, cv2.COLOR_YUV420p2GRAY)
     
     # get AT detection results
     results = detector.detect(gray)
@@ -127,106 +144,17 @@ def updatePositions():
     
     # display image
     cv2.imshow("Camera", rgb)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'): 
-        break
-    
-    
+    break
 
-### robot sockets ###
-sockets = []
-'''
-for robot in robots:
-    sockets.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
-    sockets[-1].connect((robot.IP, robot.port))
-    '''
-def write(robot, string):
-    sockets[robot].send(string.encode())
-    
-def read(robot):
-    return sockets[robot].recv(1024).decode().strip()
-
-### setup picam ###
-picam2 = Picamera2()
-config = picam2.create_preview_configuration(lores={"size": (640, 480)})   #(640, 480)
-picam2.configure(config)
-picam2.start()
-
-## adjust camera, set anchor coords ###
-while True:
-    yuv420 = picam2.capture_array("lores")
-    rgb = cv2.cvtColor(yuv420, cv2.COLOR_YUV420p2RGB)
-    gray = cv2.cvtColor(yuv420, cv2.COLOR_YUV420p2GRAY)
-    
-    ### get results of AT detection ###
-    options = apriltag.DetectorOptions(families="tag36h11")
-    detector = apriltag.Detector(options)
-    results = detector.detect(gray)
-    
-    for r in results:
-        if r.tag_id >= 0 and r.tag_id <= 1:
-            try:
-                print(str(r.center[0]) + ", " + str(r.center[1]))
-            except NameError:
-                continue
-            else:
-                anchors[r.tag_id] = Point(r.center[0], r.center[1])
-            
-    ### draw bounding boxes and info of ATs ###
-    for r in results:
-        if r.tag_id >= 0 and r.tag_id <= 1:
-            # get points
-            (ptA, ptB, ptC, ptD) = r.corners
-            ptB = (int(ptB[0]), int(ptB[1]))
-            ptC = (int(ptC[0]), int(ptC[1]))
-            ptD = (int(ptD[0]), int(ptD[1]))
-            ptA = (int(ptA[0]), int(ptA[1]))
-            (cX, cY) = (int(r.center[0]), int(r.center[1]))
-            displayTag(ptA, ptB, ptC, ptD, cX, cY)
-    
-    ### display edited image ###
-    cv2.imshow("Camera", rgb)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'): 
-        break
-    
-### get robot coords ###
-while True:
-    ### get image as RGB and GRAY ###
-    yuv420 = picam2.capture_array("lores")
-    rgb = cv2.cvtColor(yuv420, cv2.COLOR_YUV420p2RGB)
-    gray = cv2.cvtColor(yuv420, cv2.COLOR_YUV420p2GRAY)
-    
-    ### get results of AT detection ###
-    options = apriltag.DetectorOptions(families="tag36h11")
-    detector = apriltag.Detector(options)
-    results = detector.detect(gray)
-
-    for r in results:
-        if r.tag_id >= 2 and r.tag_id <= 3:
-            robots[r.tag_id-2].coords = Point((abs(anchors[0].x - r.center[0]) / abs(anchors[0].x - anchors[1].x)), (abs(anchors[0].y - r.center[1]) / abs(anchors[0].y - anchors[1].y)))
-            print("Robot " + str(r.tag_id - 2) + " relative coords: " + str(robots[r.tag_id-2].coords.x) + ", " + str(robots[r.tag_id-2].coords.y))
-    print("\n")
-   
-    ### draw bounding boxes and info of ATs ###
-    for r in results:
-        if r.tag_id >= 2 and r.tag_id <= 3:
-            # get points
-            (ptA, ptB, ptC, ptD) = r.corners
-            ptB = (int(ptB[0]), int(ptB[1]))
-            ptC = (int(ptC[0]), int(ptC[1]))
-            ptD = (int(ptD[0]), int(ptD[1]))
-            ptA = (int(ptA[0]), int(ptA[1]))
-            (cX, cY) = (int(r.center[0]), int(r.center[1]))
-            displayTag(ptA, ptB, ptC, ptD, cX, cY)
-            cv2.circle(rgb, (anchor[0].x, anchor[0].y), 5, (0, 0, 255), -1)
-            cv2.circle(rgb, (anchor[1].x, anchor[1].y), 5, (0, 0, 255), -1)
-    
-    ### display edited image ###
-    cv2.imshow("Camera", rgb)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'): 
-        break
+### main ###
+if __name__ == "__main__":
+    while True:
+        for idx in range(len(robots)):
+            robot = robots[idx]
+            robot.action()
+            velocity = drive(robot, np.array(robot.targetX, robot.targetY), obstacles, alpha=1.0, speed=0.10, obs_clearance=0.01, detect_clearance=0.10)
+            write(sockets[idx], "vx: " + velocity[0] + ", vy: " + velocity[1])
+        updatePositions()
 
 cv2.destroyAllWindows()
 picam2.stop()
