a370f9d0c4416b33f31f902a7151fffe8739365f
Chris y <123777444+chrisy2010@users.noreply.github.com>
2026-03-01 17:25:40 -0500

    Update plant_growth.ino

--- plants/plant_growth.ino
+++ plants/plant_growth.ino
@@ -1,6 +1,34 @@
 #include <Wire.h>
+#include <Arduino.h>
+
+#define IN1 X; // input 1
+#define IN2 Y; // input 2
 
 void setup() {
 
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
 
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  Wire.begin();
+  Serial.begin(9600);
+  
 }
+
+void growth() {
+  digitalWrite(IN1, HIGH);
+  delay(1000);
+  digitalWrite(IN1, LOW);
+}
+
+void loop() {
+  if (Serial.available()) {
+    char c = Serial.read();
+    if (c == 'G') {
+      growth();
+    }
+  }
+}
+
+
