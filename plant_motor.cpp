c24c98f4de3f7f023def84ed9985f450a291b6ac
George Deng <dcx11dcy17@gmail.com>
2026-03-01 17:37:17 -0500

    Implement motor control functions in plant_motor.cpp

---
+++ plant_motor.cpp
@@ -0,0 +1,30 @@
+//include files
+#include "plant_motor.h"
+
+//set motor pins
+Motor Motor{8, 9};
+
+//motor functions
+//motor mapping
+int cmap(int val, int olow, int ohigh, int mlow, int mhigh) {
+  return constrain(map(val, olow, ohigh, mlow, mhigh), mlow, mhigh);
+}
+
+//motor speed
+void Motor::speed(int val) {
+  int map_speed = cmap(abs(val), 0, 100, 0, 255);
+
+  if (val > 0) {
+    analogWrite(fpin, map_speed);
+    analogWrite(rpin, 0);
+  }
+  else {
+    analogWrite(fpin, 0);
+    analogWrite(rpin, map_speed);
+  }
+}
+
+//function: set motor speeds
+void motor(int speed) {
+  Motor.speed(speed);
+}
