f98285bf8fb37c3be195b7a7bd637bdf0c840858
George Deng <dcx11dcy17@gmail.com>
2026-03-15 16:33:17 -0400

    Update WifiComms.py

--- central/WifiComms.py
+++ central/WifiComms.py
@@ -1,27 +1,36 @@
 import socket
 import time
 
-PICO_IP = "192.168.32.172"   # StormingKids
+PICO_IP = "192.168.32.209"  # StormingKids IP
 PORT = 5000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((PICO_IP, PORT))
 print("Connected to Pico W")
-i = 0
-try:
-    while True:
-        message = str(i)+"\n"
-        i += 1
-        sock.send(message.encode())
-        print("Sent:", message.strip())
 
-        # Optional: read response
-        reply = sock.recv(1024).decode().strip()
-        print("Pico replied:", reply)
+def wifi_write(message):
+    sock.send(message.encode())
+    print("Sent:", message.strip())
+    return
+        
+def wifi_read():
+    reply = sock.recv(1024).decode().strip()
+    print("Pico replied:", reply)
+    return reply
 
-        time.sleep(1)
+if __name__ == "__main__":
+    try:
+        while True:
+            message = "Hello world\n"
+            wifi_write(message)
+            time.sleep(1)
+            reply = wifi_read()
+            print(reply)
+            
+            if 0xFF == ord('q'): 
+                break
 
-except KeyboardInterrupt:
-    print("Closing connection.")
+    except KeyboardInterrupt:
+        print("Closing connection.")
 
-sock.close()
+    sock.close()          
