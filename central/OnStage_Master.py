f4e69674dca2a979573324c92a9fb29fbbba63d6
George Deng <dcx11dcy17@gmail.com>
2026-03-22 17:04:56 -0400

    Update OnStage_Master.py

--- central/OnStage_Master.py
+++ central/OnStage_Master.py
@@ -5,13 +5,15 @@ import socket
 import cv2
 
 ### import subfiles ###
-#from CBF import get_velocity
+from OnStage_CBF import get_velocity
 from OnStage_Rcoords import updTagPos, initAnchors
-from OnStage_WifiComms import wifi_write, wifi_read
+#from OnStage_WifiComms import wifi_write, wifi_read
 
-# from munkres import Munkres, print_matrix
+from munkres import Munkres, print_matrix
 
 MAX_PLANT_GROWTH = 4
+
+states = ["None", "Waiting", "Ice", "Plant"]
 
 ### objects ###
 class Point:
@@ -26,29 +28,29 @@ class Point:
 
     def __str__(self):
         return f"Point({self.x}, {self.y})"
-
+    
 class robot:
     def __init__(self, IP, port, x, y, tag):
         self.IP = IP;
         self.port = port
         self.coords = Point(x, y)
-        self.target = Point(0, 0)
+        self.target = Point(-1, -1)
         self.rotation = 0
         self.haswater = False
-        self.state = "Ice"
+        self.state = "None"
         self.tag = tag;
     def setTarget(self, target):
         self.target = target
     def changeWater(self):
         self.haswater = not self.haswater
     def atTarget(self):
-        return (math.sqrt((self.coords.x-self.target.x)**2 + (self.coords.y-self.target.y)**2) < 0.5)  #!tentative
+        return (self.coords.distance_to(self.target) < 0.5) #!tentative
     
     def setState(self, s): #set state manually
         self.state = s
 #     def changeState(self): #change state after completing current task
 #         if (self.atTarget == True):
-#             if self.state == "Plant" and self.haswater:
+#             if self.state == "plant" and self.haswater:
 #                 if self.target.grow():
 #                     self.changeWater()
 #                     self.state = "Ice"
@@ -111,16 +113,16 @@ class ice:
 #         self.level = self.level - 1
 #         return True
 
-robots = [robot("192.168.32.172", 5000, 0, 0, 4), robot("192.168.32.172", 5000, 0, 0, 5)]   #AT tag 4-5
-anchors = [anchor(0, 0, 0), anchor(0, 0, 1), anchor(0, 0, 2), anchor(0, 0, 3)]  #AT tag 0-3
+robots = [robot("192.168.32.172", 5000, -1, -1, 4)]   #AT tag 4-5
+anchors = [anchor(-1, -1, 0), anchor(-1, -1, 1), anchor(-1, -1, 2), anchor(-1, -1, 3)]  #AT tag 0-3
 
-obstacles = [obstacle(0, 0, 0), obstacle(0, 0, 0), obstacle(0, 0, 0)]
-plants = [plant("<INSERT IP HERE>", 0, 0, 0, 8, 0)]
-icepatches = [ice(0, 0, 0)]
+obstacles = [obstacle(-1, -1, 0), obstacle(-1, -1, 0), obstacle(-1, -1, 0)]
+plants = [plant("<INSERT IP HERE>", 0, -1, -1, 5, 0)]
+icepatches = [ice(-1, -1, 0)]
 
 field_width = 8  #scales x dimension of relative coordinates
 field_length = 8 #scales y dimension of relative coordinates
-robotcount = 2  #predefined number of robots (prevent detection of extraneous tags)
+robotcount = 1  #predefined number of robots (prevent detection of extraneous tags)
 
 ### setup camera ###
 camera_port = 0
@@ -138,22 +140,46 @@ if not cam.isOpened():
 
 sockets = []
 
-def setNewTarget(robot):
-    if robot.state == "Ice":
-        for idx, ice in ice_patches:
-            if not ice_state[idx]:
-                robot.setTarget(ice)
-                return
-        robot.state = "Waiting for Ice"
-    elif robot.state == "Plant":
-        for idx, plant in plants:
-            if not plant_state[idx]:
-                robot.setTarget(plant)
-                return
-        robot.state = "Waiting for Plant"
+# def assignTargets(system): # system = plants or ice
+#     ### assign targets using matrix calculations ###
+#     # intial calculation of closest robot targets (minimal movement)
+#     # use Hungarian algorithm (O(n^3)): uses cost matrix to maximize efficiency 
+# 
+#     # initialize cost matrix
+#     matrix = []
+#     row = 0
+#     for r in robots:
+#         matrix.append([])
+#         for item in system:
+#             matrix[row].append(r.coords.distance_to(item.coords))
+#         row += 1
+#     
+#     m = Munkres()
+#     indexes = m.compute(matrix)
+#     #print_matrix(matrix, msg='Lowest cost through this matrix:')
+#     total = 0
+#     for row, column in indexes:
+#         robots[row].setTarget(system[column].coords)
+#         #value = matrix[row][column]
+#         #total += value
+#         #print(f'({row}, {column}) -> {value}')
+#     #print(f'total cost: {total}')
+        
+# def setNewTarget(robot):
+#     if robot.state == "Ice":
+#         for idx, ice in ice_patches:
+#             if not ice_state[idx]:
+#                 robot.setTarget(ice)
+#                 return
+#         robot.state = "Waiting for Ice"
+#     elif robot.state == "Plant":
+#         for idx, plant in plants:
+#             if not plant_state[idx]:
+#                 robot.setTarget(plant)
+#                 return
+#         robot.state = "Waiting for Plant"
 
     # if it doesn't find any available, we may need to just keep waiting
-        
 
 def connect(system): #systems are the arrays with objects with have .IP and .port (robots, plants)
     for item in system:
@@ -162,30 +188,34 @@ def connect(system): #systems are the arrays with objects with have .IP and .por
 
 ### main ###
 if __name__ == "__main__":
-    # initialize / get tag locations
-    while (initAnchors(cam, anchors) == False):
+    ### initialize tags/positions ###
+    # anchors
+    while (initAnchors(cam, anchors) != 1):
         print("Top left: " + str(anchors[0].coords.x) + " " + str(anchors[0].coords.y))
         print("Top right: " + str(anchors[1].coords.x) + " " + str(anchors[1].coords.y))
         print("Bottom left: " + str(anchors[2].coords.x) + " " + str(anchors[2].coords.y))
         print("Bottom right: " + str(anchors[3].coords.x) + " " + str(anchors[3].coords.y))
         continue
     
-    #******** next thing to work on ********#
-#     updTagPos(camera_port, plants, anchors, field_width)
-#     updTagPos(camera_port, robots, anchors, field_width, True)
+    # plants, robots
+    updTagPos(cam, plants, anchors, field_width)
+    updTagPos(cam, robots, anchors, field_width, True)
     
-    # intial calculation of closest robot targets (minimal movement)
-    # use Hungarian algorithm (O(n^3)): uses cost matrix to maximize efficiency 
-
-#     #initialize cost matrix
+    # ice, obstacles
+    
+#     ### assign targets using matrix calculations ###
+#     # intial calculation of closest robot targets (minimal movement)
+#     # use Hungarian algorithm (O(n^3)): uses cost matrix to maximize efficiency 
+# 
+#     # initialize cost matrix
 #     matrix = []
 #     row = 0
-#     for robot in robots:
-#         robot.setState("Ice")
+#     for r in robots:
+#         r.setState("Ice")
 #         matrix.append([])
-#         for ice in ice_patches:
-#             matrix[row].append((math.sqrt((robot.coords.x-ice.coords.x)**2 + (robot.coords.y-ice.coords.y)**2)))
-#         row+=1
+#         for i in icepatches:
+#             matrix[row].append(r.coords.distance_to(i.coords))
+#         row += 1
 #     
 #     m = Munkres()
 #     indexes = m.compute(matrix)
@@ -197,15 +227,22 @@ if __name__ == "__main__":
 #         #total += value
 #         #print(f'({row}, {column}) -> {value}')
 #     #print(f'total cost: {total}')
+
+    if (plants[0].coords.x != -1 and plants[0].coords.y != -1):
+        robots[0].setTarget(plants[0].coords)
+    
+    # testing
+    print("Robot 0 coords: " + str(robots[0].coords.x) + " " + str(robots[0].coords.y))
+    print("Plant 0 coords: " + str(plants[0].coords.x) + " " + str(plants[0].coords.y))
+    input("Press Enter to start")
         
     while True:
-#         for i in range(len(robots)):
+        for i in range(len(robots)):
 #             if robots[i].changeState(): # if we have arrived at target, we want to set a new target
 #                 setNewTarget(robots[i])
-#             velocity = get_velocity(robots[i], obstacles, alpha=1.0, speed=0.10, obs_clearance=0.01, detect_clearance=0.10)
+            velocity = get_velocity(robots[i], obstacles, alpha=1.0, speed=0.10, obs_clearance=0.01, detect_clearance=0.10)
 #             write(sockets[i], "vx: " + velocity[0] + ", vy: " + velocity[1])
         updTagPos(cam, robots, anchors, field_width, True)
-        print(str(robots[0].coords.x) + " " + str(robots[0].coords.y))  #testing
     
     cv2.destroyAllWindows()
     cam.release()
