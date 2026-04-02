3420b07f45c267437f5c3e2f2c723ee6015749d8
George Deng <dcx11dcy17@gmail.com>
2026-04-02 16:46:16 -0400

    (WIP) Refactor robot and plant initialization and logic
    Refactor robot and plant classes, update initialization and target assignment logic.

--- central/OnStage_Master.py
+++ central/OnStage_Master.py
@@ -8,12 +8,14 @@ from munkres import Munkres, print_matrix
 
 ### import subfiles ###
 from OnStage_Rcoords import updTagPos, updObsPos, initAnchors
-#from OnStage_WifiComms import wifi_write, wifi_read
+from OnStage_WifiComms import wifi_connect, wifi_write, wifi_read, wifi_disconnect
 from OnStage_pfield import pfield_path
 
 MAX_PLANT_GROWTH = 4
 
 states = ["None", "Waiting", "Ice", "Plant"]
+
+WIFI_IP = "192.168.32.209" #StormingKids
 
 ### objects ###
 class Point:
@@ -30,12 +32,13 @@ class Point:
         return f"Point({self.x}, {self.y})"
     
 class robot:
-    def __init__(self, IP, port, x, y, tag):
+    def __init__(self, IP, port, tag):
         self.IP = IP;
         self.port = port
-        self.coords = Point(x, y)
-        self.path = [[-1, -1]]
-        self.target = Point(-1, -1)
+        self.sock = None
+        self.coords = Point(-1, -1)
+        self.path = None
+        self.target = None
         self.rotation = 0
         self.haswater = False
         self.state = "None"
@@ -46,66 +49,39 @@ class robot:
         self.haswater = not self.haswater
     def atTarget(self):
         return (self.coords.distance_to(self.target) < 1) #!tentative
-    
-    def setState(self, s): #set state manually
-        self.state = s
-#     def changeState(self): #change state after completing current task
-#         if (self.atTarget == True):
-#             if self.state == "Plant" and self.haswater:
-#                 if self.target.grow():
-#                     self.changeWater()
-#                     self.state = "Ice"
-#                 else:
-#                     idx = plants.index(self.target)
-#                     plants.pop(idx)
-#                     plant_state.pop(idx)
-#                     self.state = "Plant"
-#             elif self.state == "Ice" and not self.haswater:
-#                 if self.target.withdraw():
-#                     self.changeWater()
-#                     self.state = "Plant"
-#                 else:
-#                     idx = ice_patches.index(self.target)
-#                     ice_patches.pop(idx)
-#                     ice_state.pop(idx)
-#                     self.state = "Ice"
-#             return True
-#         elif self.state == "Waiting for Ice" or self.state == "Waiting for Plant":
-#             return True
-#         else:
-#             return False
 
 class anchor:
-    def __init__(self, x, y, tag):
-        self.coords = Point(x, y)
+    def __init__(self, tag):
+        self.coords = Point(-1, -1)
         self.tag = tag
 
 class obstacle:
-    def __init__(self, x, y, radius):
-        self.coords = Point(x, y)
-        self.radius = radius
+    def __init__(self):
+        self.coords = Point(-1, -1)
+        self.radius = 0
 class plant:
-    def __init__(self, IP, port, x, y, tag, level):
-        self.IP = IP;
+    def __init__(self, IP, port, tag):
+        self.IP = IP
         self.port = port
-        self.coords = Point(x, y)
+        self.sock = None
+        self.coords = Point(-1, -1)
         self.tag = tag
         self.available = True
-        self.level = level
+        self.level = 0
     def grow(self):
         if (self.level < MAX_PLANT_GROWTH):
-            if (wifi_write("G") == -1):
-                return False
-            else:
-                return True
-        return False
+            self.level = self.level + 1
+            wifi_write(self.sock, "G")
         
 class ice:
-    def __init__(self, x, y, tag, level):
-        self.coords = Point(x, y)
+    def __init__(self, tag, level):
+        self.coords = Point(-1, -1)
         self.tag = tag
         self.available = True
         self.level = level
+    def deplete(self):
+        if (self.level > 0):
+            self.level = self.level - 1
 
 #     def withdraw(self):
 #         if (self.level <= 0):
@@ -113,12 +89,12 @@ class ice:
 #         self.level = self.level - 1
 #         return True
 
-robots = [robot("192.168.32.172", 5000, -1, -1, 4)]   #AT tag 4-5
-anchors = [anchor(-1, -1, 0), anchor(-1, -1, 1), anchor(-1, -1, 2), anchor(-1, -1, 3)]  #AT tag 0-3
+robots = [robot(WIFI_IP, 5000, 5)]   #AT tag 5
+anchors = [anchor(0), anchor(1), anchor(2)]  #AT tag 0-2
 
-obstacles = [obstacle(40, 40, 3)] #obstacle(-1, -1, 0), obstacle(-1, -1, 0), obstacle(-1, -1, 0)]
-plants = [plant("<INSERT IP HERE>", 0, -1, -1, 5, 0)]
-icepatches = [ice(-1, -1, 0, 4)]
+obstacles = [obstacle()] 
+plants = [plant(WIFI_IP, 80, 4)]  #AT tag 4
+icepatches = [ice(3, 4)]  #AT tag 3
 
 obstacle_Lhsv = [0, 30, 200]
 obstacle_Uhsv = [20, 90, 255]
@@ -140,172 +116,184 @@ cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
 if not cam.isOpened():
     print("Failed to open camera")
     exit()
+    
+def displayElements(cam, anchors, robots):
+    ret, frame = cam.read()
+    if not ret:
+        print("Failed to get camera frame")
+        return -1
+    rgb = frame
+    
+    for anchor in anchors:
+        cv2.circle(img, (anchor.coords.x, anchor.coords.y), 5, (0, 0, 255), -1)
 
-sockets = []
-
-def followPath(robot):
-    while (robot.atTarget() == False):
-        if not robot.path:
-            robot.append([robot.target.x, robot.target.y])
-        while (robot.coords.distance_to(Point(robot.path[0][0], robot.path[0][1])) >= 1):
-            updTagPos(cam, robots, anchors, field_width, True)
-            print("Robot 0 coords: " + f"{robots[0].coords.x:.2f}" + ", " + f"{robots[0].coords.y:.2f}") #testing#
-            print("Point coords: " + f"{points[idx][0]:.2f}" + ", " + f"{points[idx][1]:.2f}") #testing#
-            dx = robot.path[0][0] - robot.coords.x
-            dy = robot.path[0][1] - robot.coords.y
-            d = math.sqrt(dx**2 + dy**2)
-            print("dx: " + f"{dx:.2f}" + ", " + "dy: " + f"{dy:.2f}") #testing#
-            print("dist: " + f"{robots[0].coords.distance_to(Point(points[idx][0], points[idx][1])):.2f}") #testing#
-            print('\n') #testing#
-            V = baseV
-            Vx = V * dx / d
-            Vy = V * dy / d
-            #wifi_write(f"{Vx:.2f}" + "," + f"{Vy:.2f}")
-        robot.path.pop(0)
+    for robot in robots:
+        cv2.circle(img, (robot.coords.x, robot.coords.y), 7, (0, 100, 255), -1)
+        for i in len(robot.path):
+            cv2.circle(img, (robot.path[i][0], robot.path[i][1]), 3, (0, 255, 255), -1)
+        cv2.circle(img, (robot.target.coords.x, robot.target.coords.y), 5, (0, 255, 0), -1)
+        
+    cv2.imshow("Camera", rgb)
     return
 
-# def assignTargets(system): # system = plants or ice
-#     ### assign targets using matrix calculations ###
-#     # intial calculation of closest robot targets (minimal movement)
-#     # use Hungarian algorithm (O(n^3)): uses cost matrix to maximize efficiency 
-# 
-#     # initialize cost matrix
-#     matrix = []
-#     row = 0
-#     for r in robots:
-#         matrix.append([])
-#         for item in system:
-#             matrix[row].append(r.coords.distance_to(item.coords))
-#         row += 1
-#     
-#     m = Munkres()
-#     indexes = m.compute(matrix)
-#     #print_matrix(matrix, msg='Lowest cost through this matrix:')
-#     total = 0
-#     for row, column in indexes:
-#         robots[row].setTarget(system[column].coords)
-#         #value = matrix[row][column]
-#         #total += value
-#         #print(f'({row}, {column}) -> {value}')
-#     #print(f'total cost: {total}')
+def followPath(robot):
+    if (robot.coords.distance_to(robot.target.coords) < 1):
+        return True
+    if (len(robot.path) > 0 and robot.coords.distance_to(Point(robot.path[0][0], robot.path[0][1])) < 1):
+        robot.path.pop(0)
+    if not robot.path:
+        robot.path.append([robot.target.coords.x, robot.target.coords.y])
         
-# def setNewTarget(robot):
-#     if robot.state == "Ice":
-#         for idx, ice in ice_patches:
-#             if not ice_state[idx]:
-#                 robot.setTarget(ice)
-#                 return
-#         robot.state = "Waiting for Ice"
-#     elif robot.state == "Plant":
-#         for idx, plant in plants:
-#             if not plant_state[idx]:
-#                 robot.setTarget(plant)
-#                 return
-#         robot.state = "Waiting for Plant"
+    print("Robot coords: " + f"{robots[0].coords.x:.2f}" + ", " + f"{robots[0].coords.y:.2f}") #testing#
+    print("Next point coords: " + f"{robot.path[0][0]:.2f}" + ", " + f"{robot.path[0][1]:.2f}") #testing#
+    dx = robot.path[0][0] - robot.coords.x
+    dy = robot.path[0][1] - robot.coords.y
+    d = math.sqrt(dx**2 + dy**2)
+    print("dx: " + f"{dx:.2f}" + ", " + "dy: " + f"{dy:.2f}") #testing#
+    print("dist: " + f"{robots[0].coords.distance_to(Point(robot.path[0][0], robot.path[0][1])):.2f}") #testing#
+    print('\n') #testing#
+    V = baseV
+    Vx = V * dx / d
+    Vy = V * dy / d
+    #wifi_write(robots[0].sock, f"{Vx:.2f},{Vy:.2f}")
+    return False
 
-    # if it doesn't find any available, we may need to just keep waiting
-
-def connect(system): #systems are the arrays with objects with have .IP and .port (robots, plants)
-    for item in system:
-        sockets.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
-        sockets[-1].connect((item.IP, item.port))
-
+def assignTargets(robots, icepatches, plants): # system = plants or ice
+    ### assign targets using matrix calculations ###
+    # intial calculation of closest robot targets (minimal movement)
+    # use Hungarian algorithm (O(n^3)): uses cost matrix to maximize efficiency 
+    want_plant = []
+    want_ice = []
+    freeplant = []
+    freeice = []
+    for r in robots:
+        if (r.state == "None" or r.state == "Waiting"):
+            if r.haswater == False:
+                want_ice.append(r)
+            else:
+                want_plant.append(r)
+    for i in icepatches:
+        if (i.available == True):
+            freeice.append(i)
+    for p in plants:
+        if (p.available == True):
+            freeplant.append(p)
+    
+    # ice #
+    if (len(want_ice) > 0):
+        matrix = []
+        row = 0
+        for r in want_ice:
+            matrix.append([])
+            for i in freeice:
+                matrix[row].append(r.coords.distance_to(i.coords))
+            row += 1
+        
+        m = Munkres()
+        indexes = m.compute(matrix)
+        #print_matrix(matrix, msg='Lowest cost through this matrix:')
+        total = 0
+        for row, column in indexes:
+            want_ice[row].target = freeice[column] 
+            freeice[column].available = False
+            want_ice[row].state = "Ice"
+        for r in want_ice:
+            if r.state != "Ice":
+                r.state = "Waiting"
+    
+    # plants #
+    if (len(want_plant) > 0):
+        matrix = []
+        row = 0
+        for r in want_plant:
+            matrix.append([])
+            for p in freeplant:
+                matrix[row].append(r.coords.distance_to(p.coords))
+            row += 1
+        
+        m = Munkres()
+        indexes = m.compute(matrix)
+        #print_matrix(matrix, msg='Lowest cost through this matrix:')
+        total = 0
+        for row, column in indexes:
+            want_plant[row].target = freeplant[column] 
+            freeplant[column].available = False
+            want_plant[row].state = "Plant"
+        for r in want_plant:
+            if r.state != "Plant":
+                r.state = "Waiting"
+    return
+    
 ### main ###
 if __name__ == "__main__":
     ### initialize tags/positions ###
     # anchors
     while (initAnchors(cam, anchors) != 1):
-        print("Top left: " + str(anchors[0].coords.x) + " " + str(anchors[0].coords.y))
-        print("Top right: " + str(anchors[1].coords.x) + " " + str(anchors[1].coords.y))
-        print("Bottom left: " + str(anchors[2].coords.x) + " " + str(anchors[2].coords.y))
-        print("Bottom right: " + str(anchors[3].coords.x) + " " + str(anchors[3].coords.y))
+        for i in range(len(anchors)):
+            print(f"Anchor {i}: {anchors[i].coords.x:.2f} {anchors[i].coords.y:.2f}") #testing#
+        print("\n") #testing#
         continue
     
     # plants, ice, robots
-    while (robots[0].coords.x == -1 or robots[0].coords.y == -1 or plants[0].coords.x == -1 or plants[0].coords.y == -1):
+    counter = 0
+    while (counter != (len(robots)+len(icepatches)+len(plants))):
+        counter = 0
         updTagPos(cam, plants, anchors, field_width)
+        updTagPos(cam, icepatches, anchors, field_width)
         updTagPos(cam, robots, anchors, field_width, True)
-        print("Robot 0 coords: " + str(robots[0].coords.x) + " " + str(robots[0].coords.y))
-        print("Plant 0 coords: " + str(plants[0].coords.x) + " " + str(plants[0].coords.y))
+        for robot in robots:
+            if robot.coords != Point(-1, -1):
+                counter = counter + 1
+            print(f"Robot AT{robot.tag}: {robot.coords.x:.2f} {robot.coords.y:.2f}") #testing#
+        for ice in icepatches:
+            if ice.coords != Point(-1, -1):
+                counter = counter + 1
+            print(f"Ice AT{ice.tag}: {ice.coords.x:.2f} {ice.coords.y:.2f}") #testing#
+        for plant in plants:
+            if plant.coords != Point(-1, -1):
+                counter = counter + 1
+            print(f"Plant AT{plant.tag}: {plant.coords.x:.2f} {plant.coords.y:.2f}") #testing#
+        print("\n") #testing#
+
+#     for robot in robots:
+#         while (robot.sock == None or robot.sock == -1):
+#             robot.sock = wifi_connect(robot.IP, robot.port)
+#     for plant in plants:
+#         while (plant.sock == None or plant.sock == -1):
+#             plant.sock = wifi_connect(plant.IP, plant.port)
     
     # obstacles
-    while (updObsPos(cam, obstacles, obstacle_Lhsv, obstacle_Uhsv, anchors, field_width) == -1):
+    while (updObsPos(cam, obstacles, obstacle_Lhsv, obstacle_Uhsv, anchors, field_width) != 1):
         continue
     
-#     ### assign targets using matrix calculations ###
-#     # intial calculation of closest robot targets (minimal movement)
-#     # use Hungarian algorithm (O(n^3)): uses cost matrix to maximize efficiency 
-# 
-#     # initialize cost matrix
-#     matrix = []
-#     row = 0
-#     for r in robots:
-#         r.setState("Ice")
-#         matrix.append([])
-#         for i in icepatches:
-#             matrix[row].append(r.coords.distance_to(i.coords))
-#         row += 1
-#     
-#     m = Munkres()
-#     indexes = m.compute(matrix)
-#     #print_matrix(matrix, msg='Lowest cost through this matrix:')
-#     total = 0
-#     for row, column in indexes:
-#         robots[row].setTarget(ice_patches[column].coords.x, ice_patches[column].coords.y)
-#         #value = matrix[row][column]
-#         #total += value
-#         #print(f'({row}, {column}) -> {value}')
-#     #print(f'total cost: {total}')
-
-    if (plants[0].coords.x != -1 and plants[0].coords.y != -1):
-        robots[0].setTarget(plants[0].coords)
+    input("Press Enter to start")
+    
+    assignTargets(robots, icepatches, plants)
     
     while True:
-#             if robots[i].changeState(): # if we have arrived at target, we want to set a new target
-#                 setNewTarget(robots[i])
-            #velocity = get_velocity(robots[i], obstacles, alpha=1.0, speed=0.10, obs_clearance=0.01, detect_clearance=0.10)
-            #print("vx: " + str(velocity[0]) + ", vy: " + str(velocity[1]))
-        
         ### get path of points to target ###
-        points = pfield_path(robots[0], obstacles, field_width)
-        print(points)
+        for robot in robots:
+            if (robot.state != "None" and robot.state != "Waiting"):
+                robot.path = pfield_path(robots[0], obstacles, field_width)
+                followPath(robot)
         
-        print("\n***traversing path***") #testing#
-        ### send velocity from current position to next point ###
-        for idx in range(len(points)):
-            while (robots[0].coords.distance_to(Point(points[idx][0], points[idx][1])) >= 1):
-                updTagPos(cam, robots, anchors, field_width, True)
-                print("Robot 0 coords: " + f"{robots[0].coords.x:.2f}" + ", " + f"{robots[0].coords.y:.2f}") #testing#
-                print("Point coords: " + f"{points[idx][0]:.2f}" + ", " + f"{points[idx][1]:.2f}") #testing#
-                dx = points[idx][0] - robots[0].coords.x
-                dy = points[idx][1] - robots[0].coords.y
-                d = math.sqrt(dx**2 + dy**2)
-                print("dx: " + f"{dx:.2f}" + ", " + "dy: " + f"{dy:.2f}") #testing#
-                print("dist: " + f"{robots[0].coords.distance_to(Point(points[idx][0], points[idx][1])):.2f}") #testing#
-                print('\n') #testing#
-                V = baseV
-                Vx = V * dx / d
-                Vy = V * dy / d
-                #write(sockets[i], f"{Vx:.2f}" + "," + f"{Vy:.2f}")
-        
-        print("\n***approaching target***") #testing#
-        ### close distance to target ###
-        while (robots[0].atTarget == False):
-            updTagPos(cam, robots, anchors, field_width, True)
-            print("Robot 0 coords: " + f"{robots[0].coords.x:.2f}" + ", " + f"{robots[0].coords.y:.2f}") #testing#
-            print("Target coords: " + f"{robots[0].target.x:.2f}" + ", " + f"{robots[0].target.y:.2f}") #testing#
-            dx = robots[0].target.x - robots[0].coords.x
-            dy = robots[0].target.y - robots[0].coords.y
-            d = math.sqrt(dx**2 + dy**2)
-            print("dx: " + f"{dx:.2f}" + ", " + "dy: " + f"{dy:.2f}") #testing# 
-            print('\n') #testing#
-            V = baseV
-            Vx = V * dx / d
-            Vy = V * dy / d
-            #write(sockets[i], f"{Vx:.2f}" + "," + f"{Vy:.2f}")
-        
-        #write(sockets[i], "0,0")
-        break #switch target here
+        for robot in robots:
+            if (followPath(robot) == True):
+                if (robot.state == "Ice"):
+                    robot.target.deplete()
+                    if robot.target.level == 0:
+                        robot.target.available = False
+                    robot.haswater = True
+                if (robot.state == "Plant"):
+                    robot.target.grow()
+                    if robot.target.level == MAX_PLANT_GROWTH:
+                        robot.target.available = False
+                    robot.haswater = False
+                robot.state = "None"
+                robot.target = None
+                assignTargets(robots, icepatches, plants)
+                if (robot.state != "None" and robot.state != "Waiting"):
+                    robot.path = pfield_path(robots[0], obstacles, field_width)
     
     cv2.destroyAllWindows()
     cam.release()
