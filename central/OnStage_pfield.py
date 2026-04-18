dfa09fc65dbeca21f97bec446dc3bb1561549eba
George Deng <dcx11dcy17@gmail.com>
2026-04-18 17:34:31 -0400

    Refactor potential field functions and update parameters
    Refactor add_goal and add_obstacle functions to use numpy for efficiency. Update PLOT_TYPE and adjust parameters for boundary plot.

--- central/OnStage_pfield.py
+++ central/OnStage_pfield.py
@@ -3,77 +3,58 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import time
 
-PLOT_TYPE = "RADIUS_PLOT" #"BOUNDARY_PLOT"
+PLOT_TYPE = "BOUNDARY_PLOT" # "RADIUS_PLOT" "BOUNDARY_PLOT"
+
+mpl.rcParams['path.simplify'] = True
+mpl.rcParams['path.simplify_threshold'] = 1.0
+mpl.rcParams['agg.path.chunksize'] = 10000
 
 ### add goal object to field ###
 def add_goal (X, Y, s, r, delx, dely, loc, gridsize):
   gridsize = gridsize
-    
-  for i in range(len(X)):
-    for j in range(len(Y)):
-      d= np.sqrt((loc[0]-X[i][j])**2 + (loc[1]-Y[i][j])**2)
-      #print(f"{i} and {j}")
-      theta = np.arctan2(loc[1]-Y[i][j], loc[0] - X[i][j])
-      if d< r:
-        delx[i][j] = 0
-        dely[i][j] = 0
-      elif d>r+s:
-        delx[i][j] = gridsize * s *np.cos(theta)
-        dely[i][j] = gridsize * s *np.sin(theta)
-      else:
-        delx[i][j] = gridsize * (d-r) *np.cos(theta)
-        dely[i][j] = gridsize * (d-r) *np.sin(theta)
+  
+  d = np.sqrt((loc[0]-X)**2 + (loc[1]-Y)**2)
+  theta = np.arctan2(loc[1]-Y, loc[0]-X)
+  
+  conds = [d< r, d>r+s]
+  xchoices = [0, gridsize * s *np.cos(theta)]
+  ychoices = [0, gridsize * s *np.sin(theta)]
+  
+  delx = np.select(conds, xchoices, default = gridsize * (d-r) *np.cos(theta))
+  dely = np.select(conds, ychoices, default = gridsize * (d-r) *np.sin(theta))
+        
   return delx, dely
 
 ### add obstacle object to field ###
 def add_obstacle(X, Y, s, r, delx, dely, loc, goal, gridsize):
   obstacle = [loc[0], loc[1]]
   
-  for i in range(len(X)):
-    for j in range(len(Y)):
-      d_goal = np.sqrt((goal[0]-X[i][j])**2 + ((goal[1]-Y[i][j]))**2)
-      d_obstacle = np.sqrt((obstacle[0]-X[i][j])**2 + (obstacle[1]-Y[i][j])**2)
-      #print(f"{i} and {j}")
-      theta_goal= np.arctan2(goal[1] - Y[i][j], goal[0]  - X[i][j])
-      theta_obstacle = np.arctan2(obstacle[1] - Y[i][j], obstacle[0]  - X[i][j])
-      if d_obstacle < r:
-        delx[i][j] = -1*np.sign(np.cos(theta_obstacle))*5 +0
-        dely[i][j] = -1*np.sign(np.cos(theta_obstacle))*5  +0
-      elif d_obstacle>r+s:
-        delx[i][j] += 0 -(gridsize * s *np.cos(theta_goal))
-        dely[i][j] += 0 - (gridsize * s *np.sin(theta_goal))
-      elif d_obstacle<r+s :
-        delx[i][j] += -gridsize*3 *(s+r-d_obstacle)* np.cos(theta_obstacle) #-150
-        dely[i][j] += -gridsize*3 * (s+r-d_obstacle)*  np.sin(theta_obstacle) #-150 
-      if d_goal <r+s:
-        if delx[i][j] != 0:
-          delx[i][j]  += (gridsize * (d_goal-r) *np.cos(theta_goal))
-          dely[i][j]  += (gridsize * (d_goal-r) *np.sin(theta_goal))
-        else:
-          
-          delx[i][j]  = (gridsize * (d_goal-r) *np.cos(theta_goal))
-          dely[i][j]  = (gridsize * (d_goal-r) *np.sin(theta_goal))
-          
-      if d_goal>r+s:
-        if delx[i][j] != 0:
-          delx[i][j] += gridsize* s *np.cos(theta_goal)
-          dely[i][j] += gridsize* s *np.sin(theta_goal)
-        else:
-          
-          delx[i][j] = gridsize* s *np.cos(theta_goal)
-          dely[i][j] = gridsize* s *np.sin(theta_goal) 
-      if d_goal<r:
-          delx[i][j] = 0
-          dely[i][j] = 0
+  d_goal = np.sqrt((obstacle[0] - X)**2 + (obstacle[1] - Y)**2)
+  d_obstacle = np.sqrt((obstacle[0] - X)**2 + (obstacle[1] - Y)**2)
+                       
+  theta_goal = np.arctan2(goal[1] - Y, goal[0]  - X)
+  theta_obstacle = np.arctan2(obstacle[1] - Y, obstacle[0]  - X)
+
+  conds = [d_obstacle < r, d_obstacle>r+s, d_obstacle<r+s]
+  xchoices = [-1*np.sign(np.cos(theta_obstacle))*5, delx + 0 -(gridsize * s *np.cos(theta_goal)), delx + (-gridsize*3 *(s+r-d_obstacle)* np.cos(theta_obstacle))]
+  ychoices = [-1*np.sign(np.cos(theta_obstacle))*5, dely + 0 - (gridsize * s *np.sin(theta_goal)), dely + (-gridsize*3 * (s+r-d_obstacle)*  np.sin(theta_obstacle))]
+  
+  delx = np.select(conds, xchoices, default = delx)
+  dely = np.select(conds, ychoices, default = dely)
+  
+  delx = np.where(d_goal <r+s, delx + (gridsize * (d_goal-r) *np.cos(theta_goal)), delx)
+  dely = np.where(d_goal <r+s, dely + (gridsize * (d_goal-r) *np.sin(theta_goal)), dely)
+  
+  delx = np.where(d_goal>r+s, delx + (gridsize* s *np.cos(theta_goal)), delx)
+  dely = np.where(d_goal>r+s, dely + (gridsize* s *np.sin(theta_goal)), dely)
+  
+  delx = np.where(d_goal<r, 0, delx)
+  dely = np.where(d_goal<r, 0, dely)
    
   return delx, dely, obstacle, r
 
 ### find path based on potential field ###
 def pfield_path(robot, obstacles, field_size):
-    mpl.rcParams['path.simplify'] = True
-    mpl.rcParams['path.simplify_threshold'] = 1.0
-    mpl.rcParams['agg.path.chunksize'] = 10000
-
     gridsize = field_size
     fig, ax = plt.subplots(figsize = (gridsize,gridsize))
     
@@ -91,7 +72,7 @@ def pfield_path(robot, obstacles, field_size):
     if PLOT_TYPE == "RADIUS_PLOT":
         s = 2 #pull/push strength, tune value
     if PLOT_TYPE == "BOUNDARY_PLOT":
-        s = 6
+        s = 4
     r = 1.5 #radius/size, tune value
     delx, dely = add_goal(X, Y, s, r, delx, dely, goal, gridsize) #add goal to slope field
     ax.add_patch(plt.Circle(goal, r, color='b'))
@@ -105,14 +86,16 @@ def pfield_path(robot, obstacles, field_size):
           delx, dely, loc, r = add_obstacle(X, Y, s, r, delx, dely, loc, goal, gridsize)
           ax.add_patch(plt.Circle(loc, r, color='m')) #
       if PLOT_TYPE == "BOUNDARY_PLOT":
-        for pt in obs.path:
+        for i, pt in enumerate(obs.path):
           if pt[0] < 0 or pt[0] > field_size or pt[1] < 0 or pt[1] > field_size:
             continue
           s = 6
-          r = 0.5
+          r = 1
           loc = pt
           delx, dely, loc, r = add_obstacle(X, Y, s, r, delx, dely, loc, goal, gridsize)
+          print(f"Finished #{i}")
           ax.add_patch(plt.Circle(loc, r, color='m')) #
+        #ax.add_patch(plt.Polygon(obs.path, facecolor='m')) #
     
     ax.add_patch(plt.Circle(seek_points[0], 1, color='r'))
     ax.quiver(X, Y, delx, dely)
@@ -130,43 +113,7 @@ def pfield_path(robot, obstacles, field_size):
     for idx in range(len(segments) - start):
         point = [segments[start + idx][0][0], segments[start + idx][0][1]]
         points.append(point)
-        
-#     print(seek_points) #testing#
-#     print(points) #testing#
     
     plt.show()
     plt.close()
     return points
-
-# ### testing ###
-# class Point:
-#     def __init__(self, x, y):
-#         self.x = x
-#         self.y = y
-# 
-#     def distance_to(self, other_point):
-#         dx = self.x - other_point.x
-#         dy = self.y - other_point.y
-#         return math.sqrt(dx**2 + dy**2)
-# 
-#     def __str__(self):
-#         return f"Point({self.x}, {self.y})"
-#     
-# class robot:
-#     def __init__(self, x, y):
-#         self.coords = Point(x, y)
-#         self.target = Point(-1, -1)
-#     def setTarget(self, target):
-#         self.target = target
-# 
-# class obstacle:
-#     def __init__(self, x, y, radius):
-#         self.coords = Point(x, y)
-#         self.radius = radius
-#         
-# robots = [robot(10, 10)]
-# robots[0].setTarget(Point(60, 70))
-# obstacles = [obstacle(35.6, 37.6, 4), obstacle(47.25, 41.45, 5), obstacle(21.34, 31.34, 2.5)]
-#         
-# if __name__ == "__main__":
-#     pfield_path(robots[0], obstacles, 80)
