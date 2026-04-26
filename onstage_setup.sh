bc3b233ee9ec3609d692d6b1a51f49486e4d0977
George Deng <dcx11dcy17@gmail.com>
2026-04-26 16:42:11 -0400

    Update onstage_setup.sh

--- onstage_setup.sh
+++ onstage_setup.sh
@@ -1,13 +1,13 @@
 sudo apt update
 sudo apt install python3-pip
 
-pip install opencv-python
+sudo apt update
+sudo apt install python3-opencv libgstreamer1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good
 pip install munkres
 pip install pygame
 pip install apriltag
 sudo apt install v4l2loopback-dkms
 sudo apt install ffmpeg
-sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
 sudo apt-get install pulseaudio-module-bluetooth
 
 sudo apt install git
