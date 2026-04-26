4d45c3dc7c9a07ec25e2dd10ec5d4514c27f0110
George Deng <dcx11dcy17@gmail.com>
2026-04-26 15:28:55 -0400

    Add onstage_setup.sh for environment setup
    Set up environment by installing necessary packages and cloning the repository.

---
+++ onstage_setup.sh
@@ -0,0 +1,16 @@
+sudo apt update
+sudo apt install python3-pip
+
+pip install numpy
+pip install opencv-python
+pip install munkres
+pip install pygame
+pip install apriltag
+
+sudo apt-get install pulseaudio-module-bluetooth
+sudo apt install git
+
+cd
+mkdir onstage
+cd onstage
+git clone --depth 1 https://github.com/VedaangV/OnStage-25-26.git
