2059bfeccdd37d4b8547ecd82c02cb48295be974
George Deng <dcx11dcy17@gmail.com>
2026-06-05 18:10:27 -0400

    Simplify motor pin definitions in movement.cpp
    Removed conditional compilation for motor pin configuration.

--- robot_onstage/movement.cpp
+++ robot_onstage/movement.cpp
@@ -4,19 +4,8 @@
 
 using namespace std;
 
-//*** FIX ***//
-#define ROBOT_152
-
-//set motor pins
-#ifdef ROBOT_152
-  //***FOLLOW THIS CONFIGURATION, THIS IS THE ROBOT WITH THE PICO 2W AND THE GREEN COVER ON THE TOP***//
-  Motor Motor1{8, 9, A0}, Motor3{10, 11, A1}, Motor2{12, 13, A2}; //pico 2w .152
-
-#elif defined(ROBOT_146)
-  Motor Motor2{8, 9, A0}, Motor3{10, 11, A1}, Motor1{12, 13, A2}; //pico w .146
-#else
-#endif
-//******//
+//define motor pins
+Motor Motor1{8, 9, A0}, Motor3{10, 11, A1}, Motor2{12, 13, A2}; 
 
 #define CORRECT_ROT  //if defined, robot rotates to maintain 0 deg, else adjust formula and do not correct rotation
 
