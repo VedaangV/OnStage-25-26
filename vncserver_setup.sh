24057cb50eecd3fcf5301e214a490fc4ceb1e69e
George Deng <dcx11dcy17@gmail.com>
2026-06-05 17:40:02 -0400

    Set up VNC server with installation and configuration

--- vncserver_setup.sh
+++ vncserver_setup.sh
@@ -1,1 +1,4 @@
-
+sudo apt update && sudo apt install x11vnc -y
+x11vnc -storepasswd
+x11vnc -display :0 -forever -bg -noxdamage -repeat -rfbport 5900 -rfbauth ~/.vnc/passwd
+echo "Connect to 10.42.0.1:5900 in RealVNC"
